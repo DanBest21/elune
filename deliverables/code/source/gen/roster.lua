7264f873829126c2647be4503eac20031cc799d0
db5n17 <db5n17@soton.ac.uk>
2021-04-21 23:03:04 +0100

    Removed files that were never meant to be committed.

--- deliverables/code/source/gen/roster.lua
+++
@@ -1,3 +0,0 @@
-local menu = {{text = "Select an Option", isTitle = true}, {text = "Option 1", func = function () print("You've chosen option 1"); end}, {text = "Option 2", func = function () print("You've chosen option 2"); end}, {text = "More Options", hasArrow = true, menuList = {{text = "Option 3", func = function () print("You've chosen option 3"); end}}}}
-local menuFrame = CreateFrame("Frame", "ExampleMenuFrame", UIParent, "UIDropDownMenuTemplate")
-EasyMenu(menu, menuFrame, "cursor", 0, 0, "MENU")
